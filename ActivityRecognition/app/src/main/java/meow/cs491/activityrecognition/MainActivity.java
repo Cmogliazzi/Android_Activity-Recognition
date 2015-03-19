66eaa2b4136237acff7ef4dd82728dd91758a3ba
Cmogliazzi <cmogliazzi@gmail.com>
2015-03-19 18:25:32 -0400

    Basic GUI with countdown timer

--- ActivityRecognition/app/src/main/java/meow/cs491/activityrecognition/MainActivity.java
+++ ActivityRecognition/app/src/main/java/meow/cs491/activityrecognition/MainActivity.java
@@ -1,17 +1,46 @@
 package meow.cs491.activityrecognition;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DigitalClock;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView timer;
+
+    CountDownTimer CDT = new CountDownTimer(3000000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            timer.setText("" + String.format("%d:%d",
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+            ));
+
+
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         timer = (TextView)findViewById(R.id.tvTimer);
+
+        CDT.start();
     }
 
 
